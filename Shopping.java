7b7f6871c6121f6d6efee6cee52e4916d6ccf5fb
callu <c4043156@newcastle.ac.uk>
2025-02-24 12:49:47 +0000

    Initial commit - first class completed

---
+++ Shopping.java
@@ -0,0 +1,39 @@
+public class Shopping {
+    String item;
+    double price;
+    int quantity;
+
+    public Shopping(String item, double price, int quantity) {
+            this.item=item;
+            this.price=price;
+            this.quantity=quantity;
+    }
+
+    public String getItem() {
+        return this.item;
+    }
+
+    public double getPrice() {
+        return this.price;
+    }
+
+    public double getQuantity() {
+        return this.quantity;
+    }
+
+    public void setItem(String item) {
+        this.item=item;
+    }
+
+    public void setPrice(double price) {
+        this.price=price;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity=quantity;
+    }
+
+    public String toString() {
+        return "Item = " + getItem() + "Price = " + getPrice() + "Quantity = " + getQuantity();
+    }
+}
