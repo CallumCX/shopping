2920081c6b636e1794dd4683cc7c566aea6b2303
callu <c4043156@newcastle.ac.uk>
2025-02-25 11:46:54 +0000

    completed ItemListIO

--- Food.java
+++ Food.java
@@ -4,7 +4,7 @@ import java.util.*;
 public class Food {
     static Map<String, Double> foodDict = new HashMap<String, Double>();
 
-    public static void ReadFile() throws FileNotFoundException {
+    public static Map<String, Double> ReadFile() throws FileNotFoundException {
         File fileName = new File("Dictionary.txt");
         Scanner fileData = new Scanner(fileName);
 
@@ -14,6 +14,8 @@ public class Food {
             double priceData = Double.parseDouble(foodData[1].replace(";", ""));
             foodDict.put(nameDate, priceData);
         }
+
+        return foodDict;
     }
 }
 
--- ItemListIO.java
+++ ItemListIO.java
@@ -1,19 +1,16 @@
 import java.io.FileNotFoundException;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
-import java.util.Scanner;
+import java.util.*;
 
 public class ItemListIO {
     static List<String> allFoodItems = new ArrayList<>();
-
 
 
     public ItemListIO() {
     }
 
     public static void main(String[] args) throws FileNotFoundException {
-        Food.ReadFile();
+        Map<String, Double> dict = Food.ReadFile();
+        System.out.println(Food.foodDict);
         Scanner input = new Scanner(System.in);
         System.out.println("""
                 Menu:
@@ -32,10 +29,10 @@ public class ItemListIO {
                     getFoodList();
                     break;
                 case ("2"):
-                    addToFoodList();
+                    addToFoodList(input);
                     break;
                 case ("3"):
-                    removeFromFoodList();
+                    removeFromFoodList(input);
                     break;
                 case ("4"):
                     calculateTotalPrice();
@@ -46,14 +43,14 @@ public class ItemListIO {
             }
 
             System.out.println("""
-                Menu:
-                1.) Get food list
-                2.) Add to food list
-                3.) Remove from food list
-                4.) Calculate price
-                5.) View all food sold
-                6.) Exit
-                """);
+                    Menu:
+                    1.) Get food list
+                    2.) Add to food list
+                    3.) Remove from food list
+                    4.) Calculate price
+                    5.) View all food sold
+                    6.) Exit
+                    """);
             menuChoice = input.nextLine();
         }
     }
@@ -62,48 +59,54 @@ public class ItemListIO {
         System.out.println(allFoodItems);
     }
 
-    public static void addToFoodList() {
-        Scanner input = new Scanner(System.in);
-        System.out.println("What food item do you want to add?");
+    public static void addToFoodList(Scanner input) {
+        List<String> keyList = new ArrayList<>();
+
+        for (Map.Entry<String,Double> i:Food.foodDict.entrySet()) {
+            keyList.add(i.getKey());
+        }
+
+        System.out.println(keyList);
+
+        System.out.println("What food do you want to add?: ");
         String givenItem = input.nextLine();
 
-        if (Food.foodDict.containsKey(givenItem)) {
-            allFoodItems.add(givenItem);
-        }
+       if (keyList.contains(givenItem)) {
+        allFoodItems.add(givenItem);
+        System.out.println(givenItem + " added to the shopping list!");
+       }
         else {
-            System.out.println("What food item do you want to add?");
-            givenItem = input.nextLine();
+            System.out.println("Item is not available at this shop!");
+          return;
         }
 
     }
 
-    public static void removeFromFoodList() {
-        Scanner input = new Scanner(System.in);
-        System.out.println("What food item do you want to remove?");
+    public static void removeFromFoodList(Scanner input) {
+        System.out.println("What food do you want to remove?: ");
         String givenItem = input.nextLine();
 
         if (allFoodItems.contains(givenItem)) {
             allFoodItems.remove(givenItem);
+            System.out.println(givenItem + " has been removed from the list!");
         }
         else {
-            System.out.println("What food item do you want to remove?");
-            givenItem = input.nextLine();
+            System.out.println("Item is not available at this shop!");
+            return;
         }
     }
 
     public static void calculateTotalPrice() {
-        double overallPrice = 0.0;
-        for (String i: allFoodItems) {
-            overallPrice += Food.foodDict.get(i);
+        double totalPrice = 0.0;
+        for (String i:allFoodItems) {
+            totalPrice += Food.foodDict.get(i);
         }
-        System.out.println("Total price is: " + overallPrice);
+        System.out.println("The estimated price for the food, including VAT, is: £" + totalPrice);
     }
 
     public static void listAllFoodSold() {
         for (Map.Entry<String,Double> i: Food.foodDict.entrySet()) {
-            System.out.println("Item: " + i.getKey() + "  " + "Price: " + i.getValue());
+            System.out.println("Item: " + i.getKey() + " " + "Value: " + i.getValue());
         }
     }
-
-
 }
