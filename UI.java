c587a1089e7cf8c901103a8f29083adc184f8127
callu <c4043156@newcastle.ac.uk>
2025-02-25 12:23:00 +0000

    provided a base ui for a main window frame

---
+++ UI.java
@@ -0,0 +1,27 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class UI {
+
+    public static void main(String[] args) {
+        JFrame mainWindow = new JFrame();
+
+
+
+
+
+
+
+
+
+
+
+
+        mainWindow.setSize(750,1000);
+        mainWindow.setTitle("Shopping list creator!");
+        mainWindow.setResizable(false);
+        mainWindow.setVisible(true);
+        mainWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+
+}
